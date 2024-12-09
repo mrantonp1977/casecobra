32e7babbd5775ddb373a320d4283a11dc7e4a11c
Antonis Papaioannou <mrantonp@gmail.com>
2024-12-09 15:22:43 +0200

    Add Footer component and integrate it into the layout; enhance Home page with new content and links

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { Icons } from '@/components/Icons';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Phone from '@/components/Phone';
 import { Reviews } from '@/components/Reviews';
+import { buttonVariants } from '@/components/ui/button';
 
-import { Check, CheckCircle, StarIcon } from 'lucide-react';
+import { ArrowRight, Check, CheckCircle, StarIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -17,7 +19,9 @@ export default function Home() {
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on a{' '}
-                <span className="bg-green-600 px-2 rounded-lg text-white">Custom</span>{' '}
+                <span className="bg-green-600 px-2 rounded-lg text-white">
+                  Custom
+                </span>{' '}
                 Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
@@ -34,8 +38,8 @@ export default function Home() {
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="text-green-600 size-5 shrink-0 mr-2" />
-                    5 year print guarantee
+                    <Check className="text-green-600 size-5 shrink-0 mr-2" />5
+                    year print guarantee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="text-green-600 size-5 shrink-0 mr-2" />
@@ -199,9 +203,53 @@ export default function Home() {
       </section>
       <section>
         <MaxWidthWrapper className="py-24">
-          <div className="">
-            
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                Upload your photo and get{' '}
+                <span className="relative px-2 bg-green-600 text-white rounded-lg">
+                  your own case
+                </span>{' '}
+                now!
+              </h2>
+            </div>
           </div>
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img src="/arrow.png" alt="arrow" className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"/>
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img src="/horse.jpg" alt="horse" className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"/>
+              </div>
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="text-green-600 inline size-5 mr-1.5" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="text-green-600 inline size-5 mr-1.5" />
+              Scratch and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="text-green-600 inline size-5 mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="text-green-600 inline size-5 mr-1.5" />
+              5 year print guarantee
+            </li>
+            <div className="flex justify-center">
+              <Link href="/configure/upload" className={buttonVariants({
+                size: 'lg',
+                className: "mx-auto mt-8"
+              })}>
+                Create your case now
+                <ArrowRight className="size-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
         </MaxWidthWrapper>
       </section>
     </div>
